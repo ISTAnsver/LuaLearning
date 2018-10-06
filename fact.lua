5d60fc25bf20c6a181ff7c1e4678cc93cdcfe258
Sergey <spoom95@gmail.com>
2018-10-06 10:31:25 +0300

    Add lua factroial script

---
+++ fact.lua
@@ -0,0 +1,16 @@
+-- defines a factorial function
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        if n < 0 then
+            return n * fact(n + 1)
+        else
+            return n * fact(n - 1)
+        end
+    end
+end
+    
+print("enter a number:")
+a = io.read("*n") -- reads a number
+print(fact(a))
